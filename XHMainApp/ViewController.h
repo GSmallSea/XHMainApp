af468c44db06a869f3ecabee219674d5fb73282c
GSmallSea <gxiao_hai@163.com>
2018-11-15 17:14:55 +0800

    first commit

---
+++ XHMainApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHMainApp
+//
+//  Created by XH on 2018/11/15.
+//  Copyright © 2018年 XH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XHMainAppTests/XHMainAppTests.m
@@ -0,0 +1,39 @@
+//
+//  XHMainAppTests.m
+//  XHMainAppTests
+//
+//  Created by XH on 2018/11/15.
+//  Copyright © 2018年 XH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XHMainAppTests : XCTestCase
+
+@end
+
+@implementation XHMainAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
